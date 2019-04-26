32638340d869fe606296c39d3321c080bfefee29
Oleksii Hoshii <okeksii.hoshii@daxx.com>
2019-04-26 21:17:00 +0300

    Add unit tests

---
+++ EFInterceptors/EFInterceptors.Tests/Interceptors/FullTextInterceptorTest.cs
@@ -0,0 +1,39 @@
+﻿using EFInterceptors.Interceptors;
+using System.Data.Common;
+using System.Data.SqlClient;
+using Xunit;
+
+namespace EFInterceptors.Tests.Interceptors
+{
+    public class FullTextInterceptorTest
+    {
+        [Fact(DisplayName = "The LIKE statement should be replaced by CONTAINS statement")]
+        public void ReplaceLikeStatement()
+        {
+            var fullTextSearchInterceptor = new FullTextSearchInterceptor();
+            var dbCommand = GetDbCommand();
+
+            Assert.Contains("LIKE", dbCommand.CommandText);
+            Assert.DoesNotContain("CONTAINS", dbCommand.CommandText);
+
+            fullTextSearchInterceptor.ReaderExecuting(dbCommand, null);
+
+            Assert.DoesNotContain("LIKE", dbCommand.CommandText);
+            Assert.Contains("CONTAINS", dbCommand.CommandText);
+        }
+
+        private DbCommand GetDbCommand()
+        {
+            var sqlCommand = new SqlCommand(@"SELECT 
+                    [Extent1].[Id] AS [Id], 
+                    [Extent1].[FirstName] AS [FirstName], 
+                    [Extent1].[SecondName] AS [SecondName]
+                    FROM [dbo].[Users] AS [Extent1]
+                    WHERE [Extent1].[FirstName] LIKE @p__linq__0 ESCAPE N'~'");
+
+            sqlCommand.Parameters.AddWithValue("p__linq__0", FullTextSearchInterceptor.FullTextSearch("test"));
+
+            return sqlCommand;
+        }
+    }
+}
---
+++ EFInterceptors/EFInterceptors.Tests/Interceptors/QueryOriginInterceptorTests.cs
@@ -0,0 +1,31 @@
+﻿using EFInterceptors.Interceptors;
+using System.Data.Common;
+using System.Data.SqlClient;
+using Xunit;
+
+namespace EFInterceptors.Tests.Interceptors
+{
+    public class QueryOriginInterceptorTests
+    {
+        [Fact(DisplayName = "Stack trace should be added in the result sql query")]
+        public void StackTraceShouldBeAddedInQuery()
+        {
+            var interceptor = new QueryOriginInterceptor();
+            var dbCommand = GetDbCommand();
+
+            var lastMethodInStackTrace = nameof(this.StackTraceShouldBeAddedInQuery);
+
+            interceptor.ReaderExecuting(dbCommand, null);
+            Assert.Contains(lastMethodInStackTrace, dbCommand.CommandText);
+        }
+
+        private DbCommand GetDbCommand()
+        {
+            return new SqlCommand(@"SELECT 
+                    [Extent1].[Id] AS [Id], 
+                    [Extent1].[FirstName] AS [FirstName], 
+                    [Extent1].[SecondName] AS [SecondName]
+                    FROM [dbo].[Users] AS [Extent1]");
+        }
+    }
+}
